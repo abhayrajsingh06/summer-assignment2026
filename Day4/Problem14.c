74a0233f21e5ac867a933b0ef454b05e419449b0
abhayrajsingh06 <25114cn028@glbitm.ac.in>
2026-06-18 07:33:14 -1200

    add Day4 problems

---
+++ Day4/Problem14.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int first = 0, second = 1, next;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    if (n == 1)
+        printf("Nth Fibonacci number = %d\n", first);
+    else if (n == 2)
+        printf("Nth Fibonacci number = %d\n", second);
+    else {
+        for (i = 3; i <= n; i++) {
+            next = first + second;
+            first = second;
+            second = next;
+        }
+
+        printf("Nth Fibonacci number = %d\n", second);
+    }
+
+    return 0;
+}

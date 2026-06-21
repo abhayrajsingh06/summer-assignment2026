d165086c6b85939f25802f36f75a152e28c40e59
abhayrajsingh06 <25114cn028@glbitm.ac.in>
2026-06-21 08:57:08 -1200

    add Day20 problems bt resolved merge conflicts

---
+++ Day20/Problem78.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[100], temp;
+    int i, len;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    len = strlen(str);
+
+    // Remove newline character if present
+    if (str[len - 1] == '\n')
+    {
+        str[len - 1] = '\0';
+        len--;
+    }
+
+    // Reverse the string
+    for (i = 0; i < len / 2; i++)
+    {
+        temp = str[i];
+        str[i] = str[len - 1 - i];
+        str[len - 1 - i] = temp;
+    }
+
+    printf("Reversed string: %s\n", str);
+
+    return 0;
+}
